168ee72e20360dcb0d86728936697114722b205c
thy1411 <155474687+thy1411@users.noreply.github.com>
2024-01-08 11:31:11 +0700

    Add files via upload

---
+++ BT6/Hinhtru.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT6
+{
+    class HinhTru : HinhTron
+    {
+        private double chieuCao;
+        public HinhTru() : base()
+        {
+            chieuCao = 1.0;
+        }
+        public HinhTru(double banKinh, double chieuCao) : base(banKinh)
+        {
+            setChieuCao(chieuCao);
+        }
+        public void setChieuCao(double chieuCao)
+        {
+            this.chieuCao = Math.Abs(chieuCao);
+        }
+        public double getChieuCao()
+        {
+            return chieuCao;
+        }
+        public double tinhDienTich2()
+        {
+            return base.tinhDienTich() * chieuCao;
+        }
+        public new double tinhDienTich()
+        {
+            return 2 * base.tinhDienTich() + tinhchuvi() * chieuCao;
+        }
+    }
+}
