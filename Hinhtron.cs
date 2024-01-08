8ee45b7208489f2d0f65f708441e50db726236e5
thy1411 <155474687+thy1411@users.noreply.github.com>
2024-01-08 11:01:49 +0700

    Add files via upload

---
+++ Hinhtron.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT6
+{
+    class HinhTron
+    {
+        private double bankinh;
+        public HinhTron()
+        {
+            bankinh = 1.0;
+        }
+        public HinhTron(double bankinh)
+        {
+            this.bankinh = bankinh;
+        }
+        public void setBanKinh(double bankinh)
+        {
+            this.bankinh = Math.Abs(bankinh);
+        }
+        public double getBanKinh()
+        {
+            return bankinh;
+        }
+        public double tinhDienTich()
+        {
+            return bankinh * bankinh * Math.PI;
+        }
+        public double tinhchuvi()
+        {
+            return 2 * bankinh * Math.PI;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HinhTron htron = new HinhTron(5);
+            Console.WriteLine("*******Thông tin hình tròn: ");
+            Console.WriteLine("Ban kinh:{0} - Dien tich: {1} - Chu vi: {2}",
+                htron.getBanKinh(), htron.tinhDienTich(), htron.tinhchuvi());
+
+            HinhTru htru = new HinhTru(3, 4);
+            Console.WriteLine("*******Thông tin hình tru: ");
+            Console.WriteLine("Ban kinh:{0} - Chieu Dai: {1} - The Tich: {2}",
+                htru.getBanKinh(), htru.getChieuCao(), htru.tinhDienTich());
+
+            Console.ReadLine();
+        }
+    }
+}
